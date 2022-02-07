09f4b2edf60412c252803ec8dceae7508a124b40
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-02-07 03:21:31 -0700

    define FXWorkshopMenu, misc WorkshopMenu funcs

--- CommonLibF4/include/RE/Bethesda/IMenu.h
+++ CommonLibF4/include/RE/Bethesda/IMenu.h
@@ -903,7 +903,52 @@ namespace RE
 		static constexpr auto VTABLE{ VTABLE::WorkshopMenu };
 		static constexpr auto MENU_NAME{ "WorkshopMenu"sv };
 
-		class FXWorkshopMenu;
+		class FXWorkshopMenu :
+			public BSGFxShaderFXTarget  // 000
+		{
+		public:
+			// members
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground1;            // 0B0
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground2;            // 0B8
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground3;            // 0C0
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground4;            // 0C8
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground5;            // 0D0
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground6;            // 0D8
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground7;            // 0E0
+			msvc::unique_ptr<BSGFxShaderFXTarget> happyBackground8;            // 0E8
+			msvc::unique_ptr<BSGFxShaderFXTarget> buttonBackground;            // 0F0
+			msvc::unique_ptr<BSGFxShaderFXTarget> iconCardBackground;          // 0F8
+			msvc::unique_ptr<BSGFxShaderFXTarget> requirementsListBackground;  // 100
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessPeopleBase;         // 108
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessFoodBase;           // 110
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessWaterBase;          // 118
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessPowerBase;          // 120
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessSafetyBase;         // 128
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessBedsBase;           // 130
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessHappyValue;         // 138
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessHappyLabel;         // 140
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessHappyIcon;          // 148
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessHappyArrow;         // 150
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessSizeBase;           // 158
+			msvc::unique_ptr<BSGFxShaderFXTarget> happinessBarBrackets;        // 160
+			msvc::unique_ptr<BSGFxShaderFXTarget> requirementsBase;            // 168
+			msvc::unique_ptr<BSGFxShaderFXTarget> itemName;                    // 170
+			msvc::unique_ptr<BSGFxShaderFXTarget> selectionBracket;            // 178
+			msvc::unique_ptr<BSGFxShaderFXTarget> itemCounts;                  // 180
+			msvc::unique_ptr<BSGFxShaderFXTarget> newRecipeIcon;               // 188
+			msvc::unique_ptr<BSGFxShaderFXTarget> iconCardBracketsAndText;     // 190
+			msvc::unique_ptr<BSGFxShaderFXTarget> producesIcons;               // 198
+			msvc::unique_ptr<BSGFxShaderFXTarget> requiresIcon1;               // 1A0
+			msvc::unique_ptr<BSGFxShaderFXTarget> requiresIcon2;               // 1A8
+			msvc::unique_ptr<BSGFxShaderFXTarget> assignmentIcon;              // 1B0
+			msvc::unique_ptr<BSGFxShaderFXTarget> rowBrackets;                 // 1B8
+			msvc::unique_ptr<BSGFxShaderFXTarget> displayPath;                 // 1C0
+			msvc::unique_ptr<BSGFxShaderFXTarget> perkPanel1;                  // 1C8
+			msvc::unique_ptr<BSGFxShaderFXTarget> perkPanel2;                  // 1D0
+			msvc::unique_ptr<BSGFxShaderFXTarget> descriptionBase;             // 1D8
+			msvc::unique_ptr<BSGFxShaderFXTarget> iconBackground;              // 1E0
+		};
+		static_assert(sizeof(FXWorkshopMenu) == 0x1E8);
 
 		struct IconBG
 		{
@@ -913,6 +958,20 @@ namespace RE
 			BSReadWriteLock cachedQuadsLock;                          // 30
 		};
 		static_assert(sizeof(IconBG) == 0x38);
+
+		void CheckAndSetItemForPlacement()
+		{
+			using func_t = decltype(&WorkshopMenu::CheckAndSetItemForPlacement);
+			REL::Relocation<func_t> func{ REL::ID(1541862) };
+			return func(this);
+		}
+
+		void UpdateButtonText()
+		{
+			using func_t = decltype(&WorkshopMenu::UpdateButtonText);
+			REL::Relocation<func_t> func{ REL::ID(1089189) };
+			return func(this);
+		}
 
 		// members
 		BSTArray<NiPoint3> item3DPositions[4];                                                                          // 100
