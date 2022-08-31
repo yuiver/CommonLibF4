71ec58b9c3b89cf24eb04344112fffa52e27a67b
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-08-31 00:46:52 -0600

    add console print/printline functions

--- CommonLibF4/include/RE/Bethesda/Console.h
+++ CommonLibF4/include/RE/Bethesda/Console.h
@@ -22,6 +22,21 @@ namespace RE
 			return func(this, a_string);
 		}
 
+		void Print(const char* a_fmt, std::va_list a_args)
+		{
+			using func_t = decltype(&ConsoleLog::Print);
+			REL::Relocation<func_t> func{ REL::ID(799546) };
+			func(this, a_fmt, a_args);
+		}
+
+		void PrintLine(const char* a_fmt, ...)
+		{
+			std::va_list args;
+			va_start(args, a_fmt);
+			Print(a_fmt, args);
+			va_end(args);
+		}
+
 		// members
 		BSStringT<char> buffer;  // 08
 		bool useConsoleOverlay;  // 18
