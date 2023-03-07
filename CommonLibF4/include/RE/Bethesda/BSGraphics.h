38d411d68ab822dbb1c152f22ada27a9160ec9a8
jarari <hazardhax11@gmail.com>
2023-03-07 14:54:33 -0800

    Added TriShape and Vertex

--- CommonLibF4/include/RE/Bethesda/BSGraphics.h
+++ CommonLibF4/include/RE/Bethesda/BSGraphics.h
@@ -40,6 +40,12 @@ namespace RE
 		class RendererShadowState;
 		class Texture;
 
+		struct Buffer
+		{
+			uint64_t unk00;
+			uint64_t rawVertexData;
+		};
+
 		class ConstantGroup
 		{
 		public:
@@ -207,12 +213,130 @@ namespace RE
 		public:
 			using ResetRenderTargets_t = void (*)();
 
+			void IncRef(Buffer* vertexBuffer)
+			{
+				using func_t = decltype(&BSGraphics::Renderer::IncRef);
+				REL::Relocation<func_t> func{ REL::ID(1337764) };
+				return func(this, vertexBuffer);
+			}
+			void DecRef(Buffer* vertexBuffer)
+			{
+				using func_t = decltype(&BSGraphics::Renderer::DecRef);
+				REL::Relocation<func_t> func{ REL::ID(194808) };
+				return func(this, vertexBuffer);
+			}
+
 			// members
 			bool skipNextPresent;                     // 00
 			ResetRenderTargets_t resetRenderTargets;  // 08
 			RendererData data;                        // 10
 		};
 		static_assert(sizeof(Renderer) == 0x25D0);
+
+		struct Vertex
+		{
+			enum Attribute : std::uint8_t
+			{
+				VA_POSITION = 0x0,
+				VA_TEXCOORD0 = 0x1,
+				VA_TEXCOORD1 = 0x2,
+				VA_NORMAL = 0x3,
+				VA_BINORMAL = 0x4,
+				VA_COLOR = 0x5,
+				VA_SKINNING = 0x6,
+				VA_LANDDATA = 0x7,
+				VA_EYEDATA = 0x8,
+
+				VA_COUNT = 9
+			};
+
+			enum Flags : std::uint16_t
+			{
+				VF_VERTEX = 1 << VA_POSITION,
+				VF_UV = 1 << VA_TEXCOORD0,
+				VF_UV_2 = 1 << VA_TEXCOORD1,
+				VF_NORMAL = 1 << VA_NORMAL,
+				VF_TANGENT = 1 << VA_BINORMAL,
+				VF_COLORS = 1 << VA_COLOR,
+				VF_SKINNED = 1 << VA_SKINNING,
+				VF_LANDDATA = 1 << VA_LANDDATA,
+				VF_EYEDATA = 1 << VA_EYEDATA,
+				VF_FULLPREC = 0x400
+			};
+
+			enum Masks : std::uint64_t
+			{
+				DESC_MASK_VERT = 0xFFFFFFFFFFFFFFF0,
+				DESC_MASK_UVS = 0xFFFFFFFFFFFFFF0F,
+				DESC_MASK_NBT = 0xFFFFFFFFFFFFF0FF,
+				DESC_MASK_SKCOL = 0xFFFFFFFFFFFF0FFF,
+				DESC_MASK_DATA = 0xFFFFFFFFFFF0FFFF,
+				DESC_MASK_OFFSET = 0xFFFFFF0000000000,
+				DESC_MASK_FLAGS = ~(DESC_MASK_OFFSET)
+			};
+		};
+
+		class VertexDesc
+		{
+		public:
+			[[nodiscard]] bool HasFlag(Vertex::Flags a_flag) const
+			{
+				return ((desc >> 44) & a_flag) != 0;
+			}
+			void SetFlag(Vertex::Flags a_flag)
+			{
+				desc |= (static_cast<uint64_t>(a_flag) << 44);
+			}
+			void ClearFlag(Vertex::Flags a_flag)
+			{
+				desc &= ~(static_cast<uint64_t>(a_flag) << 44);
+			}
+
+			[[nodiscard]] std::uint32_t GetAttributeOffset(Vertex::Attribute a_attribute) const
+			{
+				return (desc >> (4 * static_cast<uint8_t>(a_attribute) + 2)) & 0x3C;
+			}
+			void SetAttributeOffset(Vertex::Attribute a_attribute, std::uint32_t a_offset)
+			{
+				if (a_attribute != Vertex::Attribute::VA_POSITION) {
+					const uint64_t lhs = static_cast<uint64_t>(a_offset) << (4 * static_cast<uint8_t>(a_attribute) + 2);
+					const uint64_t rhs = desc & ~static_cast<uint64_t>(15 << (4 * static_cast<uint8_t>(a_attribute) + 4));
+
+					desc = lhs | rhs;
+				}
+			}
+			void ClearAttributeOffsets()
+			{
+				desc &= Vertex::Masks::DESC_MASK_OFFSET;
+			}
+
+			[[nodiscard]] Vertex::Flags GetFlags() const
+			{
+				return static_cast<Vertex::Flags>((desc & Vertex::Masks::DESC_MASK_OFFSET) >> 44);
+			}
+			void SetFlags(Vertex::Flags a_flags)
+			{
+				desc |= (static_cast<uint64_t>(a_flags) << 44) | (desc & Vertex::Masks::DESC_MASK_FLAGS);
+			}
+
+			[[nodiscard]] std::uint32_t GetSize()
+			{
+				return (desc & 0xF) * 4;
+			}
+
+		private:
+			// members
+			std::uint64_t desc;  // 00
+		};
+		static_assert(sizeof(VertexDesc) == 0x8);
+
+		struct TriShape
+		{
+			VertexDesc vertexDesc;
+			Buffer* buffer08;
+			Buffer* buffer10;
+		};
+
 	};
 	static_assert(std::is_empty_v<BSGraphics>);
 
