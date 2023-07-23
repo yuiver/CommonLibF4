226f1a41f4622cf61a42c3bd981d518ac4537a27
Qudix <17361645+Qudix@users.noreply.github.com>
2023-07-22 23:06:45 -0500

    Fix compilation

--- CommonLibF4/include/RE/Bethesda/BSResource/BSResourceEnums.h
+++ CommonLibF4/include/RE/Bethesda/BSResource/BSResourceEnums.h
@@ -48,9 +48,9 @@ namespace RE::BSResource
 	{
 	public:
 		// members
-		_FILETIME modifyTime;    // 00
-		_FILETIME createTime;    // 08
-		std::uint64_t fileSize;  // 10
+		F4SE::WinAPI::_FILETIME modifyTime;  // 00
+		F4SE::WinAPI::_FILETIME createTime;  // 08
+		std::uint64_t fileSize;              // 10
 	};
 	static_assert(sizeof(Info) == 0x18);
 }
