5c9cc2b85ec8d6b0093765c3f520d746ace30adb
Qudix <17361645+Qudix@users.noreply.github.com>
2023-07-28 14:50:15 -0500

    Refactor: Remove most uses of boost (#10)
    * Remove most uses of boost
    
    * Corrected? postfix
    
    * Modify tests
    
    * chore: maintenance
    
    * Tweaks
    
    ---------
    
    Co-authored-by: Qudix <Qudix@users.noreply.github.com>

--- CommonLibF4/include/RE/Bethesda/BSTBTree.h
+++ CommonLibF4/include/RE/Bethesda/BSTBTree.h
@@ -45,25 +45,14 @@ namespace RE
 
 	private:
 		template <class U>
-		class iterator_base :
-			public boost::stl_interfaces::iterator_interface<
-				iterator_base<U>,
-				std::forward_iterator_tag,
-				U>
+		class iterator_base
 		{
-		private:
-			using super =
-				boost::stl_interfaces::iterator_interface<
-					iterator_base<U>,
-					std::forward_iterator_tag,
-					U>;
-
 		public:
-			using difference_type = typename super::difference_type;
-			using value_type = typename super::value_type;
-			using pointer = typename super::pointer;
-			using reference = typename super::reference;
-			using iterator_category = typename super::iterator_category;
+			using difference_type = std::ptrdiff_t;
+			using value_type = std::remove_const_t<U>;
+			using pointer = value_type*;
+			using reference = value_type&;
+			using iterator_category = std::forward_iterator_tag;
 
 			iterator_base() noexcept = default;
 
@@ -116,15 +105,24 @@ namespace RE
 				return _cur->entries[_pos];
 			}
 
+			[[nodiscard]] pointer operator->() const noexcept
+			{
+				return std::pointer_traits<pointer>::pointer_to(operator*());
+			}
+
 			template <class V>
 			[[nodiscard]] bool operator==(const iterator_base<V>& a_rhs) const noexcept
 			{
 				return _cur == nullptr && a_rhs._cur == nullptr;
 			}
 
-			using super::operator++;
+			template <class V>
+			[[nodiscard]] bool operator!=(const iterator_base<V>& a_rhs) const noexcept
+			{
+				return !operator==(a_rhs);
+			}
 
-			void operator++() noexcept
+			iterator_base& operator++() noexcept
 			{
 				assert(_cur != nullptr);
 				if (++_pos >= _cur->usedEntries) {
@@ -136,6 +134,14 @@ namespace RE
 						push_level();
 					}
 				}
+				return *this;
+			}
+
+			iterator_base operator++(int) noexcept
+			{
+				iterator_base tmp{ *this };
+				operator++();
+				return tmp;
 			}
 
 		protected:
--- CommonLibF4/include/RE/Bethesda/BSTList.h
+++ CommonLibF4/include/RE/Bethesda/BSTList.h
@@ -28,25 +28,14 @@ namespace RE
 		};
 
 		template <class U>
-		class iterator_base :
-			public boost::stl_interfaces::iterator_interface<
-				iterator_base<U>,
-				std::forward_iterator_tag,
-				U>
+		class iterator_base
 		{
-		private:
-			using super =
-				boost::stl_interfaces::iterator_interface<
-					iterator_base<U>,
-					std::forward_iterator_tag,
-					U>;
-
 		public:
-			using difference_type = typename super::difference_type;
-			using value_type = typename super::value_type;
-			using pointer = typename super::pointer;
-			using reference = typename super::reference;
-			using iterator_category = typename super::iterator_category;
+			using difference_type = std::ptrdiff_t;
+			using value_type = std::remove_const_t<U>;
+			using pointer = value_type*;
+			using reference = value_type&;
+			using iterator_category = std::forward_iterator_tag;
 
 			iterator_base() noexcept = default;
 			iterator_base(const iterator_base&) noexcept = default;
@@ -86,6 +75,24 @@ namespace RE
 				return get()->value;
 			}
 
+			[[nodiscard]] pointer operator->() noexcept
+			{
+				return std::pointer_traits<pointer>::pointer_to(operator*());
+			}
+
+			template <class V>
+			[[nodiscard]] bool operator==(const iterator_base<V>& a_rhs) const noexcept
+			{
+				assert(_proxy == a_rhs._proxy);
+				return _cur == a_rhs._cur;
+			}
+
+			template <class V>
+			[[nodiscard]] bool operator!=(const iterator_base<V>& a_rhs) const noexcept
+			{
+				return !operator==(a_rhs);
+			}
+
 			iterator_base& operator++() noexcept
 			{
 				if (_cur) {
@@ -97,19 +104,11 @@ namespace RE
 				return *this;
 			}
 
-			using super::operator++;
-
-			template <class V>
-			[[nodiscard]] bool operator==(const iterator_base<V>& a_rhs) noexcept
+			iterator_base operator++(int) noexcept
 			{
-				assert(_proxy == a_rhs._proxy);
-				return _cur == a_rhs._cur;
-			}
-
-			template <class V>
-			[[nodiscard]] bool operator!=(const iterator_base<V>& a_rhs) noexcept
-			{
-				return !(*this == a_rhs);
+				iterator_base tmp{ *this };
+				operator++();
+				return tmp;
 			}
 
 		protected:
