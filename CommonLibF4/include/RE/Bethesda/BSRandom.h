27cb4ddb0a5f237a0e7f904ee71182b05a2e418c
FalloutCascadia <54562300+FalloutCascadia@users.noreply.github.com>
2024-06-14 08:45:29 +0200

    Update CommonLibF4/include/RE/Bethesda/BSRandom.h
    Co-authored-by: qudix <17361645+qudix@users.noreply.github.com>

--- CommonLibF4/include/RE/Bethesda/BSRandom.h
+++ CommonLibF4/include/RE/Bethesda/BSRandom.h
@@ -4,11 +4,12 @@ namespace RE
 {
 	namespace BSRandom
 	{
-		inline std::uint32_t UnsignedInt(std::uint32_t aMin, std::uint32_t aMax)
+		inline std::uint32_t UnsignedInt(std::uint32_t a_min, std::uint32_t a_max)
 		{
 			using func_t = decltype(&BSRandom::UnsignedInt);
 			static REL::Relocation<func_t> func{ REL::ID(2267950) };
-			return (aMin, aMax);
+			return func(a_min, a_max);
+		}
 		}
 	}
 }
